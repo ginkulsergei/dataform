de17e5e5a76ffad917672a5ff3bad53fd27403e1
Serhii Hinkul <dataformtraining2024@gmail.com>
2024-05-01 21:52:20 +0000

    UPDATE : creating an incremental table_1

--- includes/constants.js
+++ includes/constants.js
@@ -1,4 +1,6 @@
 // filename is includes/constants.js
 var launch_dates = [];
 module.exports = { launch_dates };
+var staging_1 = "`dataformtraining2024.prepared_ua.staging_1`";
+module.exports = { staging_1 };
 
